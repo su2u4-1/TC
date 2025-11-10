f623d08d0feefb1205e38854c18dd6dbebfea267
su2u4 <Joey23.su@gmail.com>
2025-11-11 02:51:40 +0800

    feat: add noreturn attribute to error handling functions

--- src/tc_err.c
+++ src/tc_err.c
@@ -1,18 +1,18 @@
 #include "tc_err.h"
 
-void tcerr_alloc(void) {
+__attribute__((noreturn)) void tcerr_alloc(void) {
     fprintf(stderr, "os error: Memory allocation error\n");
     tc_free_all_memory();
     exit(1);
 }
 
-void tcerr_unclosed_string(int line_number) {
+__attribute__((noreturn)) void tcerr_unclosed_string(int line_number) {
     fprintf(stderr, "syntax error: Unclosed string at line %d\n", line_number);
     tc_free_all_memory();
     exit(1);
 }
 
-void tcerr_unclosed_comment(int line_number) {
+__attribute__((noreturn)) void tcerr_unclosed_comment(int line_number) {
     fprintf(stderr, "syntax error: Unclosed comment at line %d\n", line_number);
     tc_free_all_memory();
     exit(1);
