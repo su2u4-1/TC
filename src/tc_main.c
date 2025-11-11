bc18ab83d28fba5619fb5a6a5e10d1cb608daaed
su2u4 <Joey23.su@gmail.com>
2025-11-12 00:38:22 +0800

    refactor(tc_main): integrate Path structure for file path handling and improve output path management

--- src/tc_main.c
+++ src/tc_main.c
@@ -1,11 +1,11 @@
 #include "tc_err.h"
+#include "tc_file.h"
 #include "tc_lexer.h"
 #include "tc_lib.h"
 #include "tc_mem.h"
 
-static char* file_path = NULL;
-static char* file_name = NULL;
-static char* output_path = NULL;
+static Path* file_path = NULL;
+static Path* output_path = NULL;
 /*
  * 1: -o: output compiler result to output_path/file_name.exe
  * 2: -a: output AST to output_path/file_name.ast
@@ -14,21 +14,6 @@ static char* output_path = NULL;
  * 16: -h: display help information
  */
 static int flags = 0;
-
-static char* get_file_name(const char* path) {
-    const char* last_slash = strrchr(path, '/');
-    const char* last_backslash = strrchr(path, '\\');
-    const char* file_start = path;
-    if (last_slash != NULL && (last_backslash == NULL || last_slash > last_backslash)) {
-        file_start = last_slash + 1;
-    } else if (last_backslash != NULL) {
-        file_start = last_backslash + 1;
-    }
-    size_t len = strlen(file_start);
-    char* name = (char*)tc_malloc(len + 1);
-    strcpy(name, file_start);
-    return name;
-}
 
 static void handle_parameters(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {
@@ -44,17 +29,26 @@ static void handle_parameters(int argc, char* argv[]) {
             flags |= 16;
         } else {
             if (file_path == NULL) {
-                file_path = argv[i];
-                file_name = get_file_name(file_path);
+                file_path = path_init(argv[i]);
+#if defined(PLATFORM_WINDOWS)
+                path_windows(file_path);
+#elif defined(PLATFORM_LINUX) || defined(PLATFORM_MACOS)
+                path_linux(file_path);
+#endif
             } else if (output_path == NULL) {
-                output_path = argv[i];
+                output_path = path_init(argv[i]);
+#if defined(PLATFORM_WINDOWS)
+                path_windows(output_path);
+#elif defined(PLATFORM_LINUX) || defined(PLATFORM_MACOS)
+                path_linux(output_path);
+#endif
             }
         }
     }
 }
 
 int main(int argc, char* argv[]) {
-    printf("handle parameter\n");
+    printf("[info]: handle parameter\n");
     handle_parameters(argc, argv);
     if (flags & 16 || file_path == NULL) {
         printf("Usage: <executable_file> <source_file> [output_path] [options]\n");
@@ -66,11 +60,11 @@ int main(int argc, char* argv[]) {
         printf("  -h    Display this help information\n");
         return 0;
     }
-    printf("start compile file: %s\n", file_path);
-    printf("start read file\n");
-    FILE* file = fopen(file_path, "r");
+    printf("[info]: start compile file: %s\n", file_path->path);
+    printf("[info]: start read file\n");
+    FILE* file = fopen(file_path->path, "r");
     if (file == NULL) {
-        tcerr_file_open_error(file_path);
+        tcerr_file_open_error(file_path->path);
     }
     fseek(file, 0, SEEK_END);
     unsigned file_size = (unsigned)ftell(file);
@@ -84,13 +78,63 @@ int main(int argc, char* argv[]) {
     source_code[file_size] = '\0';
     fclose(file);
 
-    printf("start lexing\n");
+    printf("[info]: start lexing\n");
     Token* tokens = lexer(source_code);
     if (flags & 4) {
-        printf("output lexer result\n");
-        output_lexer_result(tokens, output_path, file_name);
+        printf("[info]: output lexer result\n");
+        Path* lex_path = path_init(output_path != NULL ? output_path->path : ".");
+        path_change_file_name(lex_path, file_path->file_name);
+        path_change_extension(lex_path, ".lex");
+#if defined(PLATFORM_WINDOWS)
+        path_windows(lex_path);
+#elif defined(PLATFORM_LINUX) || defined(PLATFORM_MACOS)
+        path_linux(lex_path);
+#endif
+        printf("[info]: lexer output path: %s\n", lex_path->path);
+        output_lexer_result(tokens, lex_path->path);
     }
 
     tc_free_all_memory();
     return 0;
 }
+
+// int main(void) {
+//     char* test_path[] = {
+//         "C:\\Users\\User\\Documents\\project\\file.tc",
+//         "\\c\\Users\\User\\Documents\\project\\file.tc",
+//         "/c/Users/User/Documents/project/file.tc",
+//         "/home/user/project/file.tc",
+//         "./file.tc",
+//         "../file.tc",
+//         "/usr/local/bin/file.tc",
+//         "file.tc",
+//         "D:/Games/GameDev/",
+//         "/d/Games/GameDev",
+//         "home/Games/GameDev",
+//     };
+//     for (int i = 0; i < 10; i++) {
+//         printf("Testing path: %s\n", test_path[i]);
+//         Path* p = path_init(test_path[i]);
+//         for (size_t j = 0; j < p->length; j++) {
+//             printf("'%s', ", p->components[j]);
+//         }
+//         printf("\norigin: %s", p->path);
+//         path_windows(p);
+//         printf("\nwindows: %s\n", p->path);
+//         for (size_t j = 0; j < p->length; j++) {
+//             printf("'%s', ", p->components[j]);
+//         }
+//         path_linux(p);
+//         printf("\nlinux: %s\n", p->path);
+//         for (size_t j = 0; j < p->length; j++) {
+//             printf("'%s', ", p->components[j]);
+//         }
+//         printf("\n========================================\n");
+//         tc_free(p->path);
+//         tc_free(p->components);
+//         tc_free(p->file_name);
+//         tc_free(p->extension);
+//         tc_free(p);
+//     }
+//     return 0;
+// }
