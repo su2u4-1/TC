d3ba81e9dda9acdea75d9ff762800f4c5d0b60c9
su2u4 <Joey23.su@gmail.com>
2025-11-12 14:14:18 +0800

    fix(tc_lib, grammar): add new keyword 'var' and fix grammar error

--- src/tc_lib.c
+++ src/tc_lib.c
@@ -1,7 +1,7 @@
 #include "tc_lib.h"
 
-char* tc_keywords[] = {"import", "from", "func", "class", "method", "self", "if", "elif", "else", "while", "for", "true", "false", "return", "break", "continue", "int", "float", "string", "bool", "void"};
-int tc_keywords_count = 21;
+char* tc_keywords[] = {"import", "from", "func", "class", "method", "var", "self", "if", "elif", "else", "while", "for", "true", "false", "return", "break", "continue", "int", "float", "string", "bool", "void"};
+int tc_keywords_count = 22;
 
 char* tc_symbols[] = {"(", ")", "{", "}", ",", "!", ".", "[", "]", "\"", ";", "_", "+", "-", "*", "/", "%", "==", "!=", "<", ">", "<=", ">=", "&&", "||", "=", "+=", "-=", "*=", "/=", "%="};
 int tc_symbols_count = 31;
