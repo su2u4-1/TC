0aece10b86ee8cd6781197e593efd2e63dadc86b
su2u4 <Joey23.su@gmail.com>
2025-11-16 22:55:45 +0800

    python Path maybe success

--- path.py
+++ path.py
@@ -1,71 +1,75 @@
+from typing import Literal
+
+
 class Path:
-    def __init__(self, path: str):
-        if path == "":
-            path = "./"
-        path = path.replace("\\", "/")
-        self.path = path
-        self.components = self.path.split("/")
-        if self.path.endswith("/"):
-            self.components.append("")
-        if "." in self.components[-1]:
-            self.components.extend(self.components.pop().rsplit(".", 1))
+    def __init__(self, path: str, mode: Literal["linux", "windows", "unknown"] = "unknown") -> None:
+        self.path = path.replace("\\", "/").strip()
+        self.mode = mode
+        self.header, self.components = self.path.split("/", 1) if "/" in self.path else ("", self.path)
+        self.components, self.file_name = self.components.rsplit("/", 1) if "/" in self.components else ("", self.components)
+        self.components = self.components.split("/")
+        if self.header == "" and self.path.startswith("/"):
+            self.header = "/"
+        self.file_name, self.extension = self.file_name.rsplit(".", 1) if "." in self.file_name else (self.file_name, "")
+        print(f"header: '{self.header}', components: {self.components}, file_name: '{self.file_name}', extension: '{self.extension}'")
+
+    def normalize(self) -> None:
+        normalized_components: list[str] = []
+        for component in self.components:
+            if component == ".":
+                continue
+            elif component == "..":
+                if len(normalized_components) > 0:
+                    normalized_components.pop()
+            else:
+                normalized_components.append(component)
+        self.components = normalized_components
+        if self.mode == "windows":
+            self.normalize_windows_header()
+        elif self.mode == "linux":
+            self.normalize_linux_header()
+        if len(self.components) > 0 and self.components[0] != "":
+            self.path = "/".join([self.header] + self.components + [self.file_name + (f".{self.extension}" if self.extension else "")])
         else:
-            self.components.append("")
-        if self.components[0] == "":
-            self.components.pop(0)
-            self.components[0] = "/" + self.components[0]
-        self.extension = self.components.pop()
-        if len(self.extension) > 0 and not self.extension.startswith("."):
-            self.extension = "." + self.extension
-        self.file_name = self.components.pop()
-        self.path = "/".join(self.components + [self.file_name + self.extension])
-        print(self.components, self.file_name, self.extension)
+            self.path = "/".join([self.header, self.file_name + (f".{self.extension}" if self.extension else "")])
 
-    def both(self) -> None:
-        for i in range(len(self.components)):
-            if self.components[i] == ".":
-                self.components[i] = ""
-            elif self.components[i] == ".." and i > 0:
-                self.components[i - 1] = ""
-                self.components[i] = ""
-        while "" in self.components:
-            self.components.remove("")
-        self.path = "/".join(self.components[1:] + [self.file_name + self.extension])
+    def normalize_windows_header(self) -> None:
+        if len(self.header) == 1 and self.header.isalpha():
+            self.header = self.header.upper() + ":"
+        elif len(self.header) == 2 and self.header[1].isalpha() and not self.header[0].isalpha():
+            self.header = self.header[1].upper() + ":"
+        elif len(self.header) == 2 and self.header[0].isalpha() and not self.header[1].isalpha():
+            self.header = self.header[0].upper() + ":"
+        elif self.header == "":
+            self.header = "."
+        elif self.header == "/":
+            self.header = "C:"
 
-    def windows(self) -> None:
-        if len(self.components) <= 0:
-            return
-        if len(self.components[0]) == 2 and self.components[0][0] == "/" and self.components[0][1].isalpha():
-            self.components[0] = self.components[0][1].upper() + ":"
-        elif len(self.components[0]) == 2 and self.components[0][0].isalpha() and self.components[0][1] == ":":
-            self.components[0] = self.components[0][0].upper() + ":"
-        self.both()
-        self.path = self.components[0] + "/" + self.path
-
-    def linux(self) -> None:
-        if len(self.components) <= 0:
-            return
-        if len(self.components[0]) == 2 and self.components[0][0].isalpha() and self.components[0][1] == ":":
-            self.components[0] = "/" + self.components[0][0].lower()
-        elif len(self.components[0]) == 2 and self.components[0][0] == "/" and self.components[0][1].isalpha():
-            self.components[0] = "/" + self.components[0][1].lower()
-        self.both()
-        self.path = self.components[0] + "/" + self.path
+    def normalize_linux_header(self) -> None:
+        if len(self.header) == 1 and self.header.isalpha():
+            self.header = "/" + self.header.lower()
+        elif len(self.header) == 2 and self.header[1].isalpha() and not self.header[0].isalpha():
+            self.header = "/" + self.header[1].lower()
+        elif len(self.header) == 2 and self.header[0].isalpha() and not self.header[1].isalpha():
+            self.header = "/" + self.header[0].lower()
+        elif self.header == "":
+            self.header = "."
+        elif self.header == "/":
+            self.header = ""
 
     def change_extension(self, new_extension: str) -> None:
-        if not new_extension.startswith(".") and len(new_extension) > 0:
-            self.extension = "." + new_extension
-        self.both()
-        if len(self.components) <= 0:
-            return
-        self.path = self.components[0] + "/" + self.path
+        if new_extension.startswith("."):
+            new_extension = new_extension[1:]
+        self.extension = new_extension
+        self.normalize()
 
-    def change_file_name(self, new_file_name: str) -> None:
-        self.file_name = new_file_name
-        self.both()
-        if len(self.components) <= 0:
-            return
-        self.path = self.components[0] + "/" + self.path
+    def change_file_name(self, new_file_name: str, replace_extension: bool = False) -> None:
+        if replace_extension and "." in new_file_name:
+            new_file_name, new_extension = new_file_name.rsplit(".", 1)
+            self.extension = new_extension
+        else:
+            self.file_name = new_file_name
+        self.normalize()
 
 
 paths = [
@@ -73,6 +77,7 @@ paths = [
     "/home/user/docs/report.pdf",
     "D:\\Projects\\.\\script.py",
     "../relative/../to/file.md",
+    "../relative/./to/file.md",
     "./another/relative/path/image.png",
     "/var/log/system",
     "abc",
@@ -83,13 +88,13 @@ paths = [
 ]
 
 for i in paths:
-    p = Path(i)
-    p2 = Path(i)
+    p = Path(i, mode="windows")
+    p2 = Path(i, mode="linux")
     print("original:", i)
     print("initial:", p.path)
-    p.windows()
+    p.normalize()
     print("windows:", p.path)
-    p2.linux()
+    p2.normalize()
     print("linux:", p2.path)
     p.change_extension("md")
     print("changed extension (windows):", p.path)
