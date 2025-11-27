9698ac32ebc64798b79d50c657d562acfcb619d0
su2u4 <Joey23.su@gmail.com>
2025-11-27 11:57:34 +0800

    refactor(symbol): refactor symbol table structure and initialization functions

--- include/tc_symbol.h
+++ include/tc_symbol.h
@@ -8,23 +8,60 @@
 
 /*
 SymbolTable {
-    kind: literal["global", "class", "function", "block"],
+    kind: enum[GLOBAL_SYMBOL_TABLE, CLASS_SYMBOL_TABLE, FUNCTION_SYMBOL_TABLE, BLOCK_SYMBOL_TABLE],
     parent: SymbolTable*,
+    name: string or NULL,  // if kind is CLASS_SYMBOL_TABLE or FUNCTION_SYMBOL_TABLE, name is required
+    symbols: LinkListItem*,  // map[string, Symbol*]
+}
+LinkListItem {
+    key: string,
+    value: Symbol*,
+    next: LinkListItem*,
 }
 Symbol {
     name: string,
+    kind: enum[VARIABLE_SYMBOL, FUNCTION_SYMBOL, CLASS_SYMBOL, PARAMETER_SYMBOL],
+    type: string,
 }
 */
 
-typedef struct SymbolTable {
-    struct SymbolTable* parent;
-} SymbolTable;
+typedef enum {
+    GLOBAL_SYMBOL_TABLE,
+    CLASS_SYMBOL_TABLE,
+    FUNCTION_SYMBOL_TABLE,
+    BLOCK_SYMBOL_TABLE,
+} SymbolTableKind;
+
+typedef enum {
+    VARIABLE_SYMBOL,
+    FUNCTION_SYMBOL,
+    CLASS_SYMBOL,
+    PARAMETER_SYMBOL,
+} SymbolKind;
 
 typedef struct {
     char* name;
+    SymbolKind kind;
+    char* type;
 } Symbol;
 
-SymbolTable* symbol_table_init(SymbolTable* parent);
-Symbol* symbol_init(const char* name);
+typedef struct LinkListItem {
+    char* key;
+    Symbol* value;
+    struct LinkListItem* next;
+} LinkListItem;
+
+typedef struct SymbolTable {
+    SymbolTableKind kind;
+    struct SymbolTable* parent;
+    char* name;
+    LinkListItem* symbols;
+} SymbolTable;
+
+SymbolTable* symbol_table_init(SymbolTable* parent, const char* name);
+Symbol* symbol_init(const char* name, const char* type);
+Symbol* symbol_table_get_symbol(SymbolTable* table, const char* name);
+void symbol_table_add_symbol(SymbolTable* table, Symbol* symbol);
+void symbol_table_free(SymbolTable* table);
 
 #endif  // TC_SYMBOL_H
--- src/tc_symbol.c
+++ src/tc_symbol.c
@@ -1,15 +1,63 @@
 #include "tc_symbol.h"
 
-SymbolTable* symbol_table_init(SymbolTable* parent) {
+SymbolTable* symbol_table_init(SymbolTable* parent, const char* name) {
     SymbolTable* table = (SymbolTable*)tc_malloc(sizeof(SymbolTable));
     table->parent = parent;
+    table->name = tc_strcpy(name);
+    table->symbols = NULL;
     return table;
 }
 
-Symbol* symbol_init(const char* name) {
+Symbol* symbol_init(const char* name, const char* type) {
     Symbol* symbol = (Symbol*)tc_malloc(sizeof(Symbol));
-    symbol->name = (char*)tc_malloc(strlen(name) + 1);
-    strcpy(symbol->name, name);
-    symbol->name[strlen(name)] = '\0';
+    symbol->name = tc_strcpy(name);
+    symbol->type = tc_strcpy(type);
     return symbol;
+}
+
+Symbol* symbol_table_get_symbol(SymbolTable* table, const char* name) {
+    LinkListItem* current = table->symbols;
+    while (current != NULL) {
+        if (tc_strcmp(current->key, name)) {
+            return current->value;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+void symbol_table_add_symbol(SymbolTable* table, Symbol* symbol) {
+    if (table->symbols == NULL) {
+        table->symbols = (LinkListItem*)tc_malloc(sizeof(LinkListItem));
+        table->symbols->key = tc_strcpy(symbol->name);
+        table->symbols->value = symbol;
+        table->symbols->next = NULL;
+        return;
+    }
+    LinkListItem* current = table->symbols;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    LinkListItem* new_item = (LinkListItem*)tc_malloc(sizeof(LinkListItem));
+    new_item->key = tc_strcpy(symbol->name);
+    new_item->value = symbol;
+    new_item->next = NULL;
+    current->next = new_item;
+}
+
+void symbol_table_free(SymbolTable* table) {
+    if (table) {
+        LinkListItem* current = table->symbols;
+        while (current != NULL) {
+            LinkListItem* next = current->next;
+            tc_free(current->key);
+            tc_free(current->value->name);
+            tc_free(current->value->type);
+            tc_free(current->value);
+            tc_free(current);
+            current = next;
+        }
+        tc_free(table->name);
+        tc_free(table);
+    }
 }
