af547bb3f9784871e3aa9c4b4d85d69ee3fb5e30
su2u4 <Joey23.su@gmail.com>
2026-01-21 01:00:56 +0800

    refactor(parser): redesign AST structure from linked lists to generic list containers
    Major restructure of parser AST with improved type organization:
    
    Key changes:
    - Replace manual linked list nodes with List/Node generic containers
    - Unify code and class members using union types (CodeMember, ClassMember)
    - Separate Method from Function for class methods
    - Add ElseIf structure for elif chains
    - Simplify VariableAccess and remove Arguments structure
    - Move enum definitions inside structs for better encapsulation
    - Add Scope and Name structures for scope analysis
    - Remove all parsing and output functions (preparation for rewrite)
    
    Helper functions:
    - Add list operations: create_list(), create_node(), list_append()
    - Add scope operations: create_scope(), create_name(), search()
    - Define list(type) macro for type-safe list declarations
    
    Benefits:
    - Cleaner AST traversal without manual linked list management
    - Better type safety and consistency
    - Foundation for scope analysis and semantic checks
    - More maintainable codebase with consistent patterns

--- include/parser.h
+++ include/parser.h
@@ -3,63 +3,114 @@
 
 #include "lib.h"
 
+#define list(type) offset(List(type)*)
+
+typedef struct CodeMember {
+    enum {
+        CODE_IMPORT,
+        CODE_FUNCTION,
+        CODE_CLASS
+    } type;
+    union {
+        offset(Import*) import;
+        offset(Function*) function;
+        offset(Class*) class_;
+    } content;
+} CodeMember;
+
 typedef struct Code {
-    offset(Import*) imports;
-    offset(Function*) functions;
-    offset(Class*) classes;
+    list(CodeMember*) members;
+    offset(Scope*) global_scope;
 } Code;
 
 typedef struct Import {
     string name;
     string source;
-    offset(Import*) next;
 } Import;
 
 typedef struct Function {
     string name;
     string return_type;
-    offset(Variable*) parameters;
-    offset(Statement*) body;
-    offset(Function*) next;
+    list(Variable*) parameters;
+    list(Statement*) body;
+    offset(Scope*) function_scope;
 } Function;
+
+typedef struct Method {
+    string name;
+    string return_type;
+    list(Variable*) parameters;
+    list(Statement*) body;
+    offset(Scope*) method_scope;
+} Method;
+
+typedef struct ClassMember {
+    enum {
+        CLASS_METHOD,
+        CLASS_VARIABLE
+    } type;
+    union {
+        offset(Method*) method;
+        offset(Variable*) variable;
+    } content;
+} ClassMember;
 
 typedef struct Class {
     string name;
-    offset(Function*) methods;
-    offset(Variable*) attributes;
-    offset(Class*) next;
+    list(ClassMember*) members;
+    offset(Scope*) class_scope;
 } Class;
 
 typedef struct Variable {
-    string type;
+    string var_type;
     string name;
     offset(Expression*) value;
-    offset(Variable*) next;
 } Variable;
 
-typedef enum StatementType {
-    EXPRESSION_STATEMENT,
-    VARIABLE_STATEMENT,
-    IF_STATEMENT,
-    WHILE_STATEMENT,
-    FOR_STATEMENT,
-    RETURN_STATEMENT,
-    BREAK_STATEMENT,
-    CONTINUE_STATEMENT
-} StatementType;
-
 typedef struct Statement {
-    StatementType type;
+    enum {
+        EXPRESSION_STATEMENT,
+        VARIABLE_STATEMENT,
+        IF_STATEMENT,
+        WHILE_STATEMENT,
+        FOR_STATEMENT,
+        RETURN_STATEMENT,
+        BREAK_STATEMENT,
+        CONTINUE_STATEMENT
+    } type;
     union {
         offset(Expression*) expr;
-        offset(Variable*) var_decl;
+        offset(Variable*) var;
         offset(If*) if_stmt;
         offset(While*) while_stmt;
         offset(For*) for_stmt;
-        offset(Expression*) return_stmt;
+        offset(Expression*) return_expr;
     } stmt;
-    offset(Statement*) next;
 } Statement;
+
+typedef struct If {
+    offset(Expression*) condition;
+    list(Statement*) body;
+    list(ElseIf*) else_if;
+    list(Statement*) else_body;
+} If;
+
+typedef struct ElseIf {
+    offset(Expression*) condition;
+    list(Statement*) body;
+} ElseIf;
+
+typedef struct For {
+    offset(Variable*) initializer;
+    offset(Expression*) condition;
+    offset(Expression*) increment;
+    list(Statement*) body;
+} For;
+
+typedef struct While {
+    offset(Expression*) condition;
+    list(Statement*) body;
+} While;
 
 typedef enum OperatorType {
     OP_ADD,         // +
@@ -85,81 +136,77 @@ typedef enum OperatorType {
 } OperatorType;
 
 typedef struct Expression {
-    offset(Primary*) prim;
     OperatorType operator;
-    offset(Expression*) left;
+    offset(Expression* | Primary*) left;
     offset(Expression*) right;
 } Expression;
 
-typedef struct If {
-    offset(Expression*) condition;
-    offset(Statement*) body;
-    offset(If*) else_if;
-    offset(Statement*) else_body;
-} If;
-
-typedef struct While {
-    offset(Expression*) condition;
-    offset(Statement*) body;
-} While;
-
-typedef struct For {
-    offset(Variable*) initialization;
-    offset(Expression*) condition;
-    offset(Expression*) increment;
-    offset(Statement*) body;
-} For;
-
-typedef enum PrimaryType {
-    PRIM_INTEGER,
-    PRIM_FLOAT,
-    PRIM_STRING,
-    PRIM_TRUE,
-    PRIM_FALSE,
-    PRIM_EXPRESSION,
-    PRIM_NOT_OPERAND,
-    PRIM_NEGATE_OPERAND,
-    PRIM_VARIABLE_ACCESS
-} PrimaryType;
-
 typedef struct Primary {
-    PrimaryType type;
+    enum {
+        PRIM_INTEGER,
+        PRIM_FLOAT,
+        PRIM_STRING,
+        PRIM_TRUE,
+        PRIM_FALSE,
+        PRIM_EXPRESSION,
+        PRIM_NOT_OPERAND,
+        PRIM_NEGATE_OPERAND,
+        PRIM_VARIABLE_ACCESS
+    } type;
     union {
-        string string_value;
-        offset(Expression*) expr;
-        offset(Primary*) operand;
-        offset(VariableAccess*) var_access;
+        string literal_value;         // int, float, string, true, false
+        offset(Expression*) expr;     // expr
+        offset(Primary*) operand;     // not, neg
+        offset(VariableAccess*) var;  // variable access
     } value;
 } Primary;
 
-typedef enum AccessType {
-    ACCESS_FUNCTION_CALL,
-    ACCESS_GET_ATTR,
-    ACCESS_GET_SEQ,
-    ACCESS_NONE
-} AccessType;
-
 typedef struct VariableAccess {
-    string name;
-    offset(VariableAccess*) base;
-    AccessType access_type;
+    enum {
+        VAR_NAME,
+        VAR_FUNC_CALL,
+        VAR_GET_ATTR,
+        VAR_GET_SEQ
+    } type;
     union {
-        offset(Arguments*) func_call;
-        string get_attr;
-        offset(Expression*) get_seq;
+        string name;
+        offset(VariableAccess*) var;
+    } base;
+    union {
+        list(Expression*) args;     // func call
+        string attr_name;           // get attr
+        offset(Expression*) index;  // get seq
     } access;
 } VariableAccess;
 
-typedef struct Arguments {
-    offset(Expression*) args;
-    offset(Arguments*) next;
-} Arguments;
+typedef struct Scope {
+    offset(Scope*) parent;
+    list(Name*) names;
+} Scope;
 
+typedef struct Name {
+    string name;
+    size_t id;
+} Name;
+
+typedef struct List {
+    offset(Node*) head;
+    offset(Node*) tail;
+} List;
+
+typedef struct Node {
+    offset(Node*) next;
+    offset() content;
+} Node;
+
+// public functions
 // `Code* parser(Lexer* lexer)`
 offset(Code*) parser(offset(Lexer*) lexer);
 // `int operator_precedence(string op)`
 int operator_precedence(OperatorType op);
 // `void output_ast(Code* ast_node, FILE* outfile, size_t indent)`
 void output_ast(offset(Code*) ast_node, FILE* outfile, size_t indent);
+
+#undef list
 
 #endif  // PARSER_H
