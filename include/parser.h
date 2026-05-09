b6690fc41a327ffc6994c1cf5d900cf3d7427f04
su2u4 <Joey23.su@gmail.com>
2026-05-09 16:37:11 +0800

    feat(parser): add parser implementation and structure definitions

---
+++ include/parser.h
@@ -0,0 +1,17 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "ast.h"
+#include "lexer.h"
+
+typedef struct Parser {
+    Lexer* lexer;
+    list(AST*) import_files;
+    File* file;
+    bool parse_import;  // if an import file is being parsed, skip the statement parsing and ensure that the import is only done once
+} Parser;
+
+Parser* create_parser(Lexer* lexer);
+AST* parse_code(Parser* parser);
+
+#endif  // PARSER_H
---
+++ src/parser.c
@@ -0,0 +1,339 @@
+#include "parser.h"
+
+#include "lexer.h"
+#include "lib.h"
+
+#define parser_error(message, token) fprintf(stderr, "[parser Error] at %s:%zu:%zu: %s\n", file_full_path(parser->file), token->line + 1, token->column + 1, message)
+
+Parser* create_parser(Lexer* lexer) {
+    lexer->skip_comment = true;
+    Parser* parser = create_struct(Parser);
+    parser->lexer = lexer;
+    parser->import_files = list_create();
+    parser->file = lexer->source_path;
+    parser->parse_import = false;
+    return parser;
+}
+
+static CodeMember* create_code_member(CodeMemberType type, pointer member);
+static Import* parse_import(Parser* parser);
+static Class* parse_class(Parser* parser);
+static Function* parse_function(Parser* parser);
+
+AST* parse_code(Parser* parser) {
+    AST* ast = create_struct(AST);
+    ast->members = list_create();
+    ast->file = parser->file;
+    Token* token = get_next_token(parser->lexer);
+    while (token != NULL && token->type != TOKEN_EOF) {
+        if (token->type == TOKEN_KEYWORD && token->lexeme == KEYWORD_IMPORT) {
+            list_append(ast->members, (pointer)create_code_member(CODE_IMPORT, (pointer)parse_import(parser)));
+        } else if (token->type == TOKEN_KEYWORD && token->lexeme == KEYWORD_CLASS) {
+            list_append(ast->members, (pointer)create_code_member(CODE_CLASS, (pointer)parse_class(parser)));
+        } else if (token->type == TOKEN_KEYWORD && token->lexeme == KEYWORD_FUNC) {
+            list_append(ast->members, (pointer)create_code_member(CODE_FUNCTION, (pointer)parse_function(parser)));
+        } else {
+            parser_error("Unexpected token", token);
+        }
+    }
+    return ast;
+}
+
+CodeMember* create_code_member(CodeMemberType type, pointer member) {
+    CodeMember* code_member = create_struct(CodeMember);
+    code_member->type = type;
+    switch (type) {
+        case CODE_FUNCTION:
+            code_member->member.function = (Function*)member;
+            break;
+        case CODE_CLASS:
+            code_member->member.class = (Class*)member;
+            break;
+        case CODE_IMPORT:
+            code_member->member.import = (Import*)member;
+            break;
+        default:
+            code_member->member.class = NULL;
+            break;
+    }
+    return code_member;
+}
+
+Import* parse_import(Parser* parser) {
+    Import* import = create_struct(Import);
+    Token* token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_IDENTIFIER) {
+        parser_error("Expected identifier after 'import'", token);
+        return NULL;
+    }
+    import->name = create_symbol(token->lexeme, NULL, SYMBOL_VARIABLE, NULL);
+    token = get_next_token(parser->lexer);
+    if (token->type == TOKEN_SYMBOL && token->lexeme == SYMBOL_SEMICOLON) {
+        import->path = NULL;
+        return import;
+    }
+    if (token->type != TOKEN_KEYWORD || token->lexeme != KEYWORD_FROM) {
+        parser_error("Expected 'from' after import name", token);
+        return NULL;
+    }
+    token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_STRING) {
+        parser_error("Expected string literal after 'from'", token);
+        return NULL;
+    }
+    import->path = token->lexeme;
+    return import;
+}
+
+static Method* parse_method(Parser* parser);
+static list(Variable*) parse_variable(Parser* parser);
+
+Class* parse_class(Parser* parser) {
+    Class* class = create_struct(Class);
+    Token* token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_IDENTIFIER) {
+        parser_error("Expected identifier after 'class'", token);
+        return NULL;
+    }
+    class->name = create_symbol(token->lexeme, NULL, SYMBOL_CLASS, (pointer)class);
+    token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_L_BRACE) {
+        parser_error("Expected '{' after class name", token);
+        return NULL;
+    }
+    while (true) {
+        token = get_next_token(parser->lexer);
+        if (token->type == TOKEN_SYMBOL && token->lexeme == SYMBOL_R_BRACE) {
+            break;
+        } else if (token->type == TOKEN_KEYWORD && token->lexeme == KEYWORD_METHOD) {
+            list_append(class->members, (pointer)create_code_member(CLASS_METHOD, (pointer)parse_method(parser)));
+        } else if (token->type == TOKEN_KEYWORD && token->lexeme == KEYWORD_VAR) {
+            list(Variable*) vars = parse_variable(parser);
+            while (!list_empty(vars)) {
+                list_append(class->members, (pointer)create_code_member(CLASS_ATTRIBUTE, list_pop_front(vars)));
+            }
+        } else {
+            parser_error("Unexpected token in class body", token);
+        }
+    }
+    return class;
+}
+
+#define is_builtin_type(token) (token->type == TOKEN_KEYWORD && (token->lexeme == KEYWORD_INT || token->lexeme == KEYWORD_FLOAT || token->lexeme == KEYWORD_STRING || token->lexeme == KEYWORD_BOOL || token->lexeme == KEYWORD_VOID || token->lexeme == KEYWORD_POINTER || token->lexeme == KEYWORD_CONST))
+static Symbol* parse_type(Parser* parser);
+static Statement* parse_statement(Parser* parser);
+
+Function* parse_function(Parser* parser) {
+    Function* function = create_struct(Function);
+    Token* token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_IDENTIFIER && !is_builtin_type(token)) {
+        parser_error("Expected identifier or type after 'func'", token);
+        return NULL;
+    }
+    function->type = parse_type(parser);
+    token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_IDENTIFIER) {
+        parser_error("Expected identifier after function return type", token);
+        return NULL;
+    }
+    function->name = create_symbol(token->lexeme, NULL, SYMBOL_FUNCTION, (pointer)function);
+    token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_L_PAREN) {
+        parser_error("Expected '(' after function name", token);
+        return NULL;
+    }
+    token = get_next_token(parser->lexer);
+    while (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_R_PAREN) {
+        if (token->type != TOKEN_IDENTIFIER && !is_builtin_type(token)) {
+            parser_error("Unexpected token in parameter list", token);
+            return NULL;
+        }
+        Symbol* param_type = parse_type(parser);
+        token = get_next_token(parser->lexer);
+        if (token->type != TOKEN_IDENTIFIER) {
+            parser_error("Expected identifier in parameter list", token);
+            return NULL;
+        }
+        list_append(function->parameters, (pointer)create_symbol(token->lexeme, param_type, SYMBOL_PARAMETER, NULL));
+        token = get_next_token(parser->lexer);
+        if (token->type == TOKEN_SYMBOL && token->lexeme == SYMBOL_COMMA) {
+            token = get_next_token(parser->lexer);
+        } else if (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_R_PAREN) {
+            parser_error("Expected ',' or ')' in parameter list", token);
+            return NULL;
+        }
+    }
+    token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_L_BRACE) {
+        parser_error("Expected '{' to start function body", token);
+        return NULL;
+    }
+    token = get_next_token(parser->lexer);
+    while (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_R_BRACE) {
+        Statement* stmt = parse_statement(parser);
+        if (stmt == NULL) {
+            parser_error("Unexpected token in function body", token);
+            return NULL;
+        }
+        list_append(function->body, (pointer)stmt);
+        token = get_next_token(parser->lexer);
+    }
+    return function;
+}
+
+Method* parse_method(Parser* parser) {
+    Method* method = create_struct(Method);
+    Token* token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_IDENTIFIER && !is_builtin_type(token)) {
+        parser_error("Expected identifier or type after 'method'", token);
+        return NULL;
+    }
+    method->type = parse_type(parser);
+    token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_IDENTIFIER && token->type != TOKEN_SPECIAL) {
+        parser_error("Expected identifier after method return type", token);
+        return NULL;
+    }
+    method->name = create_symbol(token->lexeme, NULL, SYMBOL_FUNCTION, (pointer)method);
+    if (token->type == TOKEN_SPECIAL) {
+        if (!is_special(token->lexeme)) {
+            parser_error("Invalid special method", token);
+            return NULL;
+        }
+        method->special = true;
+    }
+    token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_L_PAREN) {
+        parser_error("Expected '(' after method name", token);
+        return NULL;
+    }
+    token = get_next_token(parser->lexer);
+    while (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_R_PAREN) {
+        if (token->type != TOKEN_IDENTIFIER && !is_builtin_type(token)) {
+            parser_error("Unexpected token in parameter list", token);
+            return NULL;
+        }
+        Symbol* param_type = parse_type(parser);
+        token = get_next_token(parser->lexer);
+        if (token->type != TOKEN_IDENTIFIER) {
+            parser_error("Expected identifier in parameter list", token);
+            return NULL;
+        }
+        list_append(method->parameters, (pointer)create_symbol(token->lexeme, param_type, SYMBOL_PARAMETER, NULL));
+        token = get_next_token(parser->lexer);
+        if (token->type == TOKEN_SYMBOL && token->lexeme == SYMBOL_COMMA) {
+            token = get_next_token(parser->lexer);
+        } else if (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_R_PAREN) {
+            parser_error("Expected ',' or ')' in parameter list", token);
+            return NULL;
+        }
+    }
+    token = get_next_token(parser->lexer);
+    if (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_L_BRACE) {
+        parser_error("Expected '{' to start method body", token);
+        return NULL;
+    }
+    token = get_next_token(parser->lexer);
+    while (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_R_BRACE) {
+        Statement* stmt = parse_statement(parser);
+        if (stmt == NULL) {
+            parser_error("Unexpected token in method body", token);
+            return NULL;
+        }
+        list_append(method->body, (pointer)stmt);
+        token = get_next_token(parser->lexer);
+    }
+    return method;
+}
+
+static Expression* parse_expression(Parser* parser);
+
+list(Variable*) parse_variable(Parser* parser) {
+    list(Variable*) vars = list_create();
+    Symbol* type = parse_type(parser);
+    Token* token;
+    while ((token = get_next_token(parser->lexer))->type == TOKEN_IDENTIFIER) {
+        Variable* var = create_struct(Variable);
+        var->var = create_symbol(token->lexeme, type, SYMBOL_VARIABLE, NULL);
+        token = get_next_token(parser->lexer);
+        if (token->type == TOKEN_SYMBOL && token->lexeme == SYMBOL_ASSIGN) {
+            get_next_token(parser->lexer);  // consume '='
+            var->initializer = parse_expression(parser);
+            list_append(vars, (pointer)var);
+            token = get_next_token(parser->lexer);
+        }
+        if (token->type == TOKEN_SYMBOL && token->lexeme == SYMBOL_COMMA) {
+            continue;
+        } else if (token->type == TOKEN_SYMBOL && token->lexeme == SYMBOL_SEMICOLON) {
+            break;
+        } else {
+            parser_error("Expected ',' or ';' after variable declaration", token);
+            break;
+        }
+    }
+    return vars;
+}
+
+Symbol* parse_type(Parser* parser) {
+    Symbol* type;
+    Token* token = get_current_token(parser->lexer);
+    if (token->type != TOKEN_IDENTIFIER && !is_builtin_type(token)) {
+        parser_error("Expected type", token);
+        return NULL;
+    }
+    if (token->type == TOKEN_KEYWORD) {
+        if (token->lexeme == KEYWORD_INT) {
+            type = symbol_int;
+        } else if (token->lexeme == KEYWORD_FLOAT) {
+            type = symbol_float;
+        } else if (token->lexeme == KEYWORD_STRING) {
+            type = symbol_string;
+        } else if (token->lexeme == KEYWORD_BOOL) {
+            type = symbol_bool;
+        } else if (token->lexeme == KEYWORD_VOID) {
+            type = symbol_void;
+        } else if (token->lexeme == KEYWORD_POINTER) {
+            type = symbol_pointer;
+        } else if (token->lexeme == KEYWORD_CONST) {
+            type = symbol_const;
+        } else {
+            parser_error("Unknown built-in type", token);
+            type = NULL;
+        }
+    } else if (token->type == TOKEN_IDENTIFIER) {
+        type = create_symbol(token->lexeme, NULL, SYMBOL_TYPE, NULL);
+    } else {
+        parser_error("Unexpected token when parsing type", token);
+        type = NULL;
+    }
+    token = peek_next_token(parser->lexer);
+    if (token->type == TOKEN_SYMBOL && token->lexeme == SYMBOL_LT) {
+        get_next_token(parser->lexer);  // consume '<'
+        token = get_next_token(parser->lexer);
+        if (token->type != TOKEN_IDENTIFIER && !is_builtin_type(token)) {
+            parser_error("Expected type in container type", token);
+            return NULL;
+        }
+        Symbol* element_type = parse_type(parser);
+        if (element_type == NULL) {
+            return NULL;
+        }
+        type->type = element_type;
+        token = get_next_token(parser->lexer);
+        if (token->type != TOKEN_SYMBOL || token->lexeme != SYMBOL_GT) {
+            parser_error("Expected '>' after container type", token);
+            return NULL;
+        }
+    }
+    return type;
+}
+
+Statement* parse_statement(Parser* parser) {
+    Statement* stmt = create_struct(Statement);
+    return stmt;
+}
+
+Expression* parse_expression(Parser* parser) {
+    Expression* exp = create_struct(Expression);
+    return exp;
+}
