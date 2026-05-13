24053ae60a424d7385291bc241517e2c9f1bc2b1
su2u4 <Joey23.su@gmail.com>
2026-05-13 23:41:31 +0800

    feat(tools): enhance test detection and command line argument parsing in runtest scripts

--- tools/runtest.bat
+++ tools/runtest.bat
@@ -8,24 +8,50 @@ if errorlevel 1 (
     exit /b 1
 )
 
+REM Detect available tests
+set "MAX_TEST_INDEX=0"
+for /d %%D in (.\test\test*) do (
+    set "TEST_NAME=%%~nxD"
+    set "TEST_INDEX=!TEST_NAME:test=!"
+    set "INVALID_TEST="
+    if not defined TEST_INDEX set "INVALID_TEST=1"
+    for /f "delims=0123456789" %%A in ("!TEST_INDEX!") do set "INVALID_TEST=1"
+    if not defined INVALID_TEST (
+        set "TEST_DIR_!TEST_INDEX!=%%~nxD"
+        if !TEST_INDEX! gtr !MAX_TEST_INDEX! set "MAX_TEST_INDEX=!TEST_INDEX!"
+    )
+)
+
 REM Parse command line arguments
 set "RUN_ALL=1"
-set "RUN_TEST1=0"
-set "RUN_TEST2=0"
-set "RUN_TEST3=0"
-set "RUN_TEST4=0"
 set "CLEAR_SCREEN=0"
 
 if "%~1"=="" goto run_tests
 
-set "RUN_ALL=0"
 :parse_args
 if "%~1"=="" goto run_tests
-if "%~1"=="-c" set "CLEAR_SCREEN=1"
-if "%~1"=="-1" set "RUN_TEST1=1"
-if "%~1"=="-2" set "RUN_TEST2=1"
-if "%~1"=="-3" set "RUN_TEST3=1"
-if "%~1"=="-4" set "RUN_TEST4=1"
+set "ARG=%~1"
+if /I "!ARG!"=="-c" (
+    set "CLEAR_SCREEN=1"
+) else if "!ARG:~0,1!"=="-" (
+    set "TEST_INDEX=!ARG:~1!"
+    set "INVALID_TEST="
+    if not defined TEST_INDEX set "INVALID_TEST=1"
+    for /f "delims=0123456789" %%A in ("!TEST_INDEX!") do set "INVALID_TEST=1"
+    if defined INVALID_TEST (
+        echo Unknown option: !ARG!
+        echo Usage: %~nx0 [-c] [-1] [-2] [-3] ...
+        endlocal
+        exit /b 1
+    )
+    set "RUN_ALL=0"
+    set "RUN_TEST_!TEST_INDEX!=1"
+) else (
+    echo Unknown option: !ARG!
+    echo Usage: %~nx0 [-c] [-1] [-2] [-3] ...
+    endlocal
+    exit /b 1
+)
 shift
 goto parse_args
 
@@ -39,51 +65,38 @@ echo.
 echo ========================================
 
 if "%RUN_ALL%"=="1" (
-    set "RUN_TEST1=1"
-    set "RUN_TEST2=1"
-    set "RUN_TEST3=1"
-    set "RUN_TEST4=1"
-    echo Running all tests...
-    echo ========================================
-)
-
-if "%RUN_TEST1%"=="1" (
-    echo.
-    echo [Test 1]
-    .\build\program.exe .\test\test1\test1.tc
-    if errorlevel 1 (
-        echo ERROR: Test 1 failed!
-        set "HAS_ERROR=1"
+    if "%MAX_TEST_INDEX%"=="0" (
+        echo No tests found.
+    ) else (
+        echo Running all tests...
+        echo ========================================
+        for /l %%I in (1,1,%MAX_TEST_INDEX%) do (
+            if defined TEST_DIR_%%I (
+                set "TEST_NAME=!TEST_DIR_%%I!"
+                echo.
+                echo [Test %%I]
+                .\build\program.exe .\test\!TEST_NAME!\!TEST_NAME!.tc -l -a -s
+                if errorlevel 1 (
+                    echo ERROR: Test %%I failed!
+                    set "HAS_ERROR=1"
+                )
+            )
+        )
     )
-)
-
-if "%RUN_TEST2%"=="1" (
-    echo.
-    echo [Test 2]
-    .\build\program.exe .\test\test2\test2.tc
-    if errorlevel 1 (
-        echo ERROR: Test 2 failed!
-        set "HAS_ERROR=1"
-    )
-)
-
-if "%RUN_TEST3%"=="1" (
-    echo.
-    echo [Test 3]
-    .\build\program.exe .\test\test3\test3.tc
-    if errorlevel 1 (
-        echo ERROR: Test 3 failed!
-        set "HAS_ERROR=1"
-    )
-)
-
-if "%RUN_TEST4%"=="1" (
-    echo.
-    echo [Test 4]
-    .\build\program.exe .\test\test4\test4.tc
-    if errorlevel 1 (
-        echo ERROR: Test 4 failed!
-        set "HAS_ERROR=1"
+) else (
+    for /l %%I in (1,1,%MAX_TEST_INDEX%) do (
+        if defined RUN_TEST_%%I (
+            if defined TEST_DIR_%%I (
+                set "TEST_NAME=!TEST_DIR_%%I!"
+                echo.
+                echo [Test %%I]
+                .\build\program.exe .\test\!TEST_NAME!\!TEST_NAME!.tc -l -a -s
+                if errorlevel 1 (
+                    echo ERROR: Test %%I failed!
+                    set "HAS_ERROR=1"
+                )
+            )
+        )
     )
 )
 
--- tools/runtest.sh
+++ tools/runtest.sh
@@ -7,43 +7,53 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Detect available tests
+MAX_TEST_INDEX=0
+TEST_NAMES=()
+for test_path in ./test/test*; do
+    [ -d "$test_path" ] || continue
+
+    test_name=${test_path##*/}
+    test_index=${test_name#test}
+    if [[ ! $test_index =~ ^[0-9]+$ ]]; then
+        continue
+    fi
+
+    TEST_NAMES[$test_index]="$test_name"
+    if (( test_index > MAX_TEST_INDEX )); then
+        MAX_TEST_INDEX=$test_index
+    fi
+done
+
 # Parse command line arguments
 RUN_ALL=1
-RUN_TEST1=0
-RUN_TEST2=0
-RUN_TEST3=0
-RUN_TEST4=0
 CLEAR_SCREEN=0
+SELECTED_TESTS=()
 
 if [ $# -eq 0 ]; then
     RUN_ALL=1
 else
-    RUN_ALL=0
     for arg in "$@"; do
         case $arg in
             -c)
                 CLEAR_SCREEN=1
                 ;;
-            -1)
-                RUN_TEST1=1
-                ;;
-            -2)
-                RUN_TEST2=1
-                ;;
-            -3)
-                RUN_TEST3=1
-                ;;
-            -4)
-                RUN_TEST4=1
+            -[0-9]*)
+                test_index=${arg#-}
+                if [[ ! $test_index =~ ^[0-9]+$ ]]; then
+                    echo "Unknown option: $arg"
+                    echo "Usage: $0 [-c] [-1] [-2] [-3] ..."
+                    exit 1
+                fi
+
+                RUN_ALL=0
+                SELECTED_TESTS[$test_index]=1
                 ;;
             *)
                 echo "Unknown option: $arg"
-                echo "Usage: $0 [-c] [-1] [-2] [-3] [-4]"
+                echo "Usage: $0 [-c] [-1] [-2] [-3] ..."
                 echo "  -c    Clear screen before running tests"
-                echo "  -1    Run test1"
-                echo "  -2    Run test2"
-                echo "  -3    Run test3"
-                echo "  -4    Run test4"
+                echo "  -N    Run testN"
                 echo "  (no options: run all tests)"
                 exit 1
                 ;;
@@ -60,53 +70,35 @@ HAS_ERROR=0
 echo ""
 echo "========================================"
 if [ $RUN_ALL -eq 1 ]; then
-    RUN_TEST1=1
-    RUN_TEST2=1
-    RUN_TEST3=1
-    RUN_TEST4=1
-    echo "Running all tests..."
-    echo "========================================"
-fi
-
-if [ $RUN_TEST1 -eq 1 ]; then
-    echo ""
-    echo "[Test 1]"
-    ./build/program ./test/test1/test1.tc
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Test 1 failed!"
-        HAS_ERROR=1
+    if [ $MAX_TEST_INDEX -eq 0 ]; then
+        echo "No tests found."
+    else
+        echo "Running all tests..."
+        echo "========================================"
     fi
 fi
 
-if [ $RUN_TEST2 -eq 1 ]; then
-    echo ""
-    echo "[Test 2]"
-    ./build/program ./test/test2/test2.tc
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Test 2 failed!"
-        HAS_ERROR=1
+for ((i=1; i<=MAX_TEST_INDEX; i++)); do
+    test_name=${TEST_NAMES[$i]}
+    if [ -z "$test_name" ]; then
+        continue
     fi
-fi
 
-if [ $RUN_TEST3 -eq 1 ]; then
-    echo ""
-    echo "[Test 3]"
-    ./build/program ./test/test3/test3.tc
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Test 3 failed!"
-        HAS_ERROR=1
+    should_run=0
+    if [ $RUN_ALL -eq 1 ] || [ -n "${SELECTED_TESTS[$i]}" ]; then
+        should_run=1
     fi
-fi
 
-if [ $RUN_TEST4 -eq 1 ]; then
-    echo ""
-    echo "[Test 4]"
-    ./build/program ./test/test4/test4.tc
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Test 4 failed!"
-        HAS_ERROR=1
+    if [ $should_run -eq 1 ]; then
+        echo ""
+        echo "[Test $i]"
+        ./build/program "./test/$test_name/$test_name.tc" -l -a -s
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Test $i failed!"
+            HAS_ERROR=1
+        fi
     fi
-fi
+done
 
 echo ""
 echo "========================================"
